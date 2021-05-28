3488dfe9d634e3e8994d7f2eb03f135f210b530f
DHejduk <d.hejduk99@gmail.com>
2021-05-28 22:37:55 +0200

    user entity added

---
+++ src/main/java/com/onlineshop/project/entity/User.java
@@ -0,0 +1,21 @@
+package com.onlineshop.project.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    private String userName;
+    private String email;
+    private String password;
+}
