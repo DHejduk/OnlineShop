de3b53f2a6ed716b64471d2cc55ec6198f2acff3
DHejduk <d.hejduk99@gmail.com>
2021-06-21 14:44:38 +0200

    services update

--- src/main/java/com/onlineshop/project/service/UserServiceImpl.java
+++ src/main/java/com/onlineshop/project/service/UserServiceImpl.java
@@ -1,69 +1,58 @@
-package com.onlineshop.project.service.impl;
+package com.onlineshop.project.service;
 
 import com.onlineshop.project.model.entity.User;
 import com.onlineshop.project.repository.UserRepository;
-import com.onlineshop.project.security.SecurityConfig;
-import com.onlineshop.project.service.UserService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Service @Transactional
-@AllArgsConstructor
-public class UserServiceImpl implements UserService {
+@RequiredArgsConstructor
+public class UserServiceImpl{
 
     @Autowired
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    @Override
+
     public void save(User user) {
         userRepository.save(user);
     }
 
-    @Override
+
     public User findById(Long id) {
         return userRepository.findUserByUserId(id);
     }
 
-    @Override
     public User findByEmail(String username) {
         return userRepository.findByEmail(username);
     }
 
-
-    @Override
     public User findByUserNameAndPassword(String username, String password) {
         return userRepository.findByUserNameAndPassword(username,password);
     }
 
-    @Override
     public String encodePassword(String password) {
         return passwordEncoder.encoder().encode(password);
     }
 
-
     public boolean passwordMatches( String given,String encoded){
-//        return securityConfig.bCryptPasswordEncoder().matches(given,encoded);
         return passwordEncoder.encoder().matches(given, encoded);
     }
 
     public User getUser(){
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();        String username;
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username;
+
         if (principal instanceof UserDetails){
             username = ((UserDetails)principal).getUsername();
         } else {
             username = principal.toString();
         }
+
         return  this.findByEmail(username);
-
     }
-
 }
